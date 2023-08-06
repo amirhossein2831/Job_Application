100165cbc486111f5ebd4f0511a1327791d2c7a1
Amirhossein <Amirmemool12@gmail.com>
2023-08-06 19:34:18 +0330

    :card_file_box: DB: add company field to DB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('firstName',255);
             $table->string('lastName',255);
+            $table->string('company')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
