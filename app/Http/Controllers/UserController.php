5ee9f47cd9c6755f24224848127b236c7123867b
Amirhossein <Amirmemool12@gmail.com>
2023-08-06 14:50:41 +0330

    :hammer: chore: create user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,6 +14,6 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        echo "ok";
+        return User::create($request->all());
     }
 }
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -10,29 +10,30 @@
     <div class="container" style="display: flex;justify-content: center;align-items: center;width: 1000px;margin-top: 20px;height: 400px;border-radius: 10px;border: 1px solid black">
         <form class="row g-3" action="/register/seeker" method="post">
             @csrf
+            <input type="hidden" name="user_type" value="seeker">
             <div class="col-md-6">
                 <label class="form-label">FirstName
-                    <input type="text" class="form-control" style="width: 480px">
+                    <input type="text" name="firstName" class="form-control" style="width: 480px">
                 </label>
             </div>
             <div class="col-md-6">
                 <label class="form-label">Last Name
-                    <input type="text" class="form-control"style="width: 480px">
+                    <input type="text" name="lastName" class="form-control"style="width: 480px">
                 </label>
             </div>
             <div class="col-md-6">
                 <label class="form-label">Password
-                    <input type="password" class="form-control" style="width: 480px">
+                    <input type="password" name="password" class="form-control" style="width: 480px">
                 </label>
             </div>
             <div class="col-md-6">
                 <label class="form-label">Confirm Password
-                    <input type="password" class="form-control"style="width: 480px">
+                    <input type="password" name="confirmPassword" class="form-control"style="width: 480px">
                 </label>
             </div>
             <div class="mb-3">
                 <label for="emailInput" class="form-label">Email</label>
-                <input type="email" class="form-control" id="emailInput" placeholder="Amir@gmail.com">
+                <input type="email" name="email" class="form-control" id="emailInput" placeholder="Amir@gmail.com">
             </div>
             <div class="col-12">
                 <button type="submit" class="btn btn-primary">Sign in</button>
