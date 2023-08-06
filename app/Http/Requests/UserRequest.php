a6cf50971075db593f09e33ce73b101cfded3b83
Amirhossein <Amirmemool12@gmail.com>
2023-08-06 15:04:35 +0330

    :heavy_plus_sign: feat: create UserRequest Class'

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'firstName'=>'required|string|max:24|min|8',
+            'lastName'=>'required|string|max:24|min|8',
+            'password'=>'required|string|max:24|min|8|confirmed',
+            'confirmPassword' => 'required|same:password|min:8',
+            'email'=>'required|email'
+        ];
+    }
+}
