ccddee1062be8cb861cf837fbfead3d3c86bf334
Amirhossein <Amirmemool12@gmail.com>
2023-08-06 23:44:43 +0330

    :lipstick::recycle: fix: delete some :poop: code and use component to create the field

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -9,20 +9,10 @@
          style="display: flex;justify-content: center;align-items: center;width: 1000px;height: 450px;margin-top: 20px;border-radius: 10px;border: 1px solid black">
         <form class="row g-3" action="/login" method="post">
             @csrf
-            <div class="mb-3">
-                <label for="emailInput" class="form-label">Email</label>
-                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-                       id="emailInput" placeholder="Amir@gmail.com">
-                <x-errors.field-message name="email"/>
-            </div>
+            <x-field field-name="email" type="email" label="Email" container-class="mb-3">Amir@gmail.com</x-field>
 
-            <div class="mb-3">
-                <label class="form-label">Password
-                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                           style="width: 480px">
-                </label>
-                <x-errors.field-message name="password"/>
-            </div>
+            <x-field field-name="password" type="password" label="Password" container-class="col-md-6"/>
+
             <p>you don't have account? &nbsp;<a href="/register/seeker">register</a></p>
             <div class="col-12">
                 <button type="submit" class="btn btn-primary">Sign in</button>
