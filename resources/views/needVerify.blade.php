b0689b06c519aa38ff9b3d8306cc728fa6f3093a
Amirhossein <Amirmemool12@gmail.com>
2023-08-07 13:28:28 +0330

    :heavy_plus_sign: feat: create need verify view

---
+++ resources/views/needVerify.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('contact')
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="card-header">Verify Account</div>
+            <div class="card-body">
+                <p>please verify you account first</p>
+            </div>
+        </div>
+    </div>
+@endsection
