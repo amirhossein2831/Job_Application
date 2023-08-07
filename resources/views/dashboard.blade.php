f7cb910cfd3c4d5093c14a04ecc5e76e16d88abc
Amirhossein <Amirmemool12@gmail.com>
2023-08-07 14:17:28 +0330

    :lipstick: sytle: show the user that your trial expired or not

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,11 @@
         <div class="row justify-content-center">
            <p> Hello, {{auth()->user()->firstName}}</p>
             @if(Auth::check() && auth()->user()->user_type === "employee")
-                <p>Your trial will expire on {{auth()->user()->user_trial}}</p>
+                @if(now()->format('Y-m-d') > auth()->user()->user_trial)
+                    <p>Your trial was expired on {{auth()->user()->user_trial}}</p>
+                @else
+                    <p>Your trial will expire on {{auth()->user()->user_trial}}</p>
+                @endif
             @endif
             <x-items.dashboard-item label="User Profile" class="primary"/>
             <x-items.dashboard-item label="User Profile" class="primary"/>
