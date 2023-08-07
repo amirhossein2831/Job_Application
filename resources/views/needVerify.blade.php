fdd2991dbc99c8a8434793cd193e3216e59f9862
Amirhossein <Amirmemool12@gmail.com>
2023-08-07 14:11:27 +0330

    :lipstick: sytle: add a resend email button

--- resources/views/needVerify.blade.php
+++ resources/views/needVerify.blade.php
@@ -4,7 +4,8 @@
         <div class="row justify-content-center mt-5">
             <div class="card-header">Verify Account</div>
             <div class="card-body">
-                <p>please verify you account first</p>
+                <p>please verify you account first to access the dashboard</p>
+                <a href="/resend/email/verify">resend email</a>
             </div>
         </div>
     </div>
