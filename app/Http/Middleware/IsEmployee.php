26ca2f601e4f16579d8b48a7a64a26268fe9032c
Amirhossein <Amirmemool12@gmail.com>
2023-08-07 21:34:02 +0330

    :heavy_plus_sign: feat: create is employee middleware

---
+++ app/Http/Middleware/IsEmployee.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class IsEmployee
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        $user = Auth::user();
+        if ($user->user_type !== 'employee') {
+            return redirect('/');
+        }
+        return $next($request);
+    }
+}
