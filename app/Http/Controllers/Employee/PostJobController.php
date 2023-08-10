9f4f09158a68c8d85676b2c56ae178566f019e5f
Amirhossein <Amirmemool12@gmail.com>
2023-08-10 18:27:08 +0330

    :heavy_plus_sign: feat: create Post controller

---
+++ app/Http/Controllers/Employee/PostJobController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Employee;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostJobController extends Controller
+{
+    public function create()
+    {
+        return "hello";
+    }
+}
