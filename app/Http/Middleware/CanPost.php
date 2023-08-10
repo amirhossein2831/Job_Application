36d282cd959a3771630d592a406a73b561d18462
Amirhossein <Amirmemool12@gmail.com>
2023-08-10 18:46:57 +0330

    :heavy_plus_sign: feat: create new MiddleWare to stop person create a Post withOut Premium account

---
+++ app/Http/Middleware/CanPost.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CanPost
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        $user = Auth::user();
+        $trial = $user->user_trial;
+        $billingEnd = $user->billing_ends;
+        if ($trial < now()->format('Y-m-d') && is_null($billingEnd)) {
+            return redirect('/pay/subscription')->with('success', 'you need to make your account premium');
+        }else if ($trial < now()->format('Y-m-d') && $billingEnd < now()->format('Y-m-d')) {
+            return redirect('/pay/subscription')->with('success', 'your premium account time has finish perches new one');
+        }
+        return $next($request);
+    }
+}
