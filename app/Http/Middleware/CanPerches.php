3897c75e801e863543cb283068d9757ba42286e9
Amirhossein <Amirmemool12@gmail.com>
2023-08-10 18:46:19 +0330

    :heavy_plus_sign: feat: create new MiddleWare to stop person perches a box twice

---
+++ app/Http/Middleware/CanPerches.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CanPerches
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        $user = Auth::user();
+        $billingEnd = $user->billing_ends;
+        if ($billingEnd > now()->format('Y-m-d')) {
+            return redirect('/')->with('success','you already perches a Box');
+        }
+        return $next($request);
+    }
+}
