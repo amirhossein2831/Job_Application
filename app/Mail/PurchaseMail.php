2d8f2905caf6d4008bd6d01b5cd842880e614dd7
Amirhossein <Amirmemool12@gmail.com>
2023-08-10 19:56:44 +0330

    :heavy_plus_sign: feat: create Mail to send email after purchsage

---
+++ app/Mail/PurchaseMail.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PurchaseMail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public array $plan;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($plan)
+    {
+        $this->plan = $plan;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return Envelope
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Purchase Notify Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return Content
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.notify',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
