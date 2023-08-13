b0ca7415c9a6da5f0dbc369f8e329a93355ab781
Amirhossein <Amirmemool12@gmail.com>
2023-08-13 16:50:58 +0330

    :hammer: chore: make the input field component general

--- resources/views/components/input-field.blade.php
+++ resources/views/components/input-field.blade.php
@@ -1,4 +1,16 @@
-<div class="form-group">
-    <label for="{{$id}}">{{$label}}</label>
-    <input type="{{$type}}" name="{{$name}}" id="{{$id}}" class="form-control">
-</div>
+@if($type === 'radio')
+    <div class="form-check">
+        <input type="radio" class="form-check-input" name="job_type" id="{{$id}}" value="{{$name}}">
+        <label for="{{$id}}" class="form-check-label">{{$label}}</label>
+    </div>
+@elseif($type === 'textarea')
+    <div class="form-group  mt-2">
+        <label for="{{$id}}">{{$label}}</label>
+            <textarea name="{{$name}}" id="{{$id}}" class="form-control"></textarea>
+    </div>
+@else
+    <div class="form-group  mt-2">
+        <label for="{{$id}}">{{$label}}</label>
+        <input type="{{$type}}" name="{{$name}}" id="{{$id}}" class="form-control">
+    </div>
+@endif
