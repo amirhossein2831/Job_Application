cfe2509cfa26e22f47f45548f52d7373b4b92833
Amirhossein <Amirmemool12@gmail.com>
2023-08-13 17:29:48 +0330

    :lipstick: style: add the validation error to the UI

--- resources/views/components/input-field.blade.php
+++ resources/views/components/input-field.blade.php
@@ -1,16 +1,28 @@
 @if($type === 'radio')
     <div class="form-check">
-        <input type="radio" class="form-check-input" name="job_type" id="{{$id}}" value="{{$name}}">
+        <input type="radio" class="form-check-input @error($name) is-invalid @enderror" name="job_type" id="{{$id}}" value="{{$name}}"{{$slot}}>
         <label for="{{$id}}" class="form-check-label">{{$label}}</label>
     </div>
 @elseif($type === 'textarea')
-    <div class="form-group  mt-2">
+    <div class="form-group mt-2">
         <label for="{{$id}}">{{$label}}</label>
-            <textarea name="{{$name}}" id="{{$id}}" class="form-control"></textarea>
+            <textarea name="{{$name}}" id="{{$id}}" class="form-control @error($name) is-invalid @enderror"></textarea>
     </div>
 @else
-    <div class="form-group  mt-2">
+    <div class="form-group mt-2">
         <label for="{{$id}}">{{$label}}</label>
-        <input type="{{$type}}" name="{{$name}}" id="{{$id}}" class="form-control">
+        <input type="{{$type}}" name="{{$name}}" id="{{$id}}" class="form-control @error($name) is-invalid @enderror">
     </div>
 @endif
+<x-errors.field-message name="{{$name}}"/>
+<style>
+    .is-invalid{
+        border: 2px solid red;
+    }
+    .error-text {
+        color: #F51212;
+        font-family: "Century Schoolbook L" ,sans-serif;
+        font-size: .9em;
+        font-weight: bold;
+    }
+</style>
