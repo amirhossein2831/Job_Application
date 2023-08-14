4ad7b6e758c8112297e975dd622e64e60c6cda7b
Amirhossein <Amirmemool12@gmail.com>
2023-08-14 15:21:32 +0330

    :hammer: chore: pass user to view

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function index()
     {
-        return view('profile');
+        return view('profile', [
+            'user'=> Auth::user()
+        ]);
     }
 }
