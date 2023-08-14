76c303b1c21770068a1d9559c9eb1138459c5134
Amirhossein <Amirmemool12@gmail.com>
2023-08-14 17:30:54 +0330

    :lipstick: style: add edit and delete button to the job list

--- resources/views/job/allJob.blade.php
+++ resources/views/job/allJob.blade.php
@@ -23,6 +23,7 @@
                                 <th>Salary</th>
                                 <th>Close Date</th>
                                 <th>Image</th>
+                                <th>Action</th>
                             </tr>
                             </thead>
                             <tfoot>
@@ -35,6 +36,8 @@
                                 <th>Salary</th>
                                 <th>Close Date</th>
                                 <th>Image</th>
+                                <th>Action</th>
+
                             </tr>
                             </tfoot>
                             <tbody>
@@ -48,6 +51,15 @@
                                         <td>{{$job->salary}}</td>
                                         <td>{{$job->close_date}}</td>
                                         <td><img style="width: 50px;height: 30px" src="{{\Illuminate\Support\Facades\Storage::url($job->post_image)}}" alt=""></td>
+                                        <td>
+                                            <form  style="display: inline"  action="/job/delete" method="post">
+                                                @csrf
+                                                @method("DELETE")
+                                                <input type="hidden" name="delete" value="{{$job->id}}">
+                                                <button style="border: 8px;height: 30px;color: red;background: #dee2e6" type="submit">Delete</button>
+                                            </form>
+                                            <a style="color: dodgerblue;font-size: 16px;padding: 7px 12px;text-decoration: none;background: #dee2e6" href="/job/edit/{{$job->id}}">edit</a>
+                                        </td>
                                     </tr>
                                 @endforeach
                             </tbody>
