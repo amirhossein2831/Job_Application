d587d468b16fdc9556d1f650ce912a26e9d2db91
Amirhossein <Amirmemool12@gmail.com>
2023-08-14 15:21:13 +0330

    :lipstick: style: add user information to profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,12 +3,18 @@
 @section('contact')
     <section class="home">
         <div class="home-content">
-            <h1>Hi,I am amirhossein</h1>
-            <h3>web developer</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus, alias amet animi debitis eveniet,
-                modi nisi nulla possimus quae quasi quia quos, recusandae saepe sapiente? Asperiores at distinctio in
-                numquam odio officiis qui! Aut, eius illo ipsa possimus rem saepe voluptatem voluptatibus! Cupiditate
-                est ipsa, nesciunt temporibus ut velit!</p>
+            <h1>Hi,I'm {{$user->firstName.' '.$user->lastName}}</h1>
+            <h3>Resume: web developer</h3>
+            <p class="about">About: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolorem dolores error
+                exercitationem expedita maiores, molestias nisi, nulla obcaecati officia optio possimus quibusdam
+                quisquam quo repellat, sequi similique velit veniam?</p>
+            <p>Email: {{$user->email}}</p>
+            <p>Account Type: {{$user->user_type}}</p>
+            @if($user->user_type === 'employee')
+                <p>Company: {{$user->company}}</p>
+                <p>Trial End: {{$user->billing_ends}}</p>
+                <p>Premium End: {{$user->billing_ends}}</p>
+            @endif
             <a href="" class="btn-hire">Hire Me</a>
         </div>
         <div class="home-image">
@@ -16,9 +22,7 @@
                 <span></span>
                 <span></span>
                 <div class="image">
-                    <img
-                        src="{{\Illuminate\Support\Facades\Storage::url('image/ywvsgvDYAuAGT0GjVxDJPoLdwlnFby7ivSatqAvh.jpg')}}"
-                        alt="">
+                    <img src="{{\Illuminate\Support\Facades\Storage::url($user->profile_pic ?? 'image/Default-Profile.png')}}" alt="">
                 </div>
             </div>
         </div>
