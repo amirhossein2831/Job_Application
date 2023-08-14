233ade7a3fac784bd1b6ecb73c055c2fb275deda
Amirhossein <Amirmemool12@gmail.com>
2023-08-14 19:57:29 +0330

    :heavy_plus_sign: feat: create a request for change password

---
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ChangePasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'old_password' => 'required|min:8|max:24',
+            'new_password'=>'required|min:8|max:24',
+            'confirmPassword'=>'required|same:new_password'
+        ];
+    }
+}
