f1aeb2586754e6f4703a0c297ea0cf150ea5841b
Amirhossein <Amirmemool12@gmail.com>
2023-08-14 18:53:13 +0330

    :hammer: chore: update the Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateProfileRequest;
 use Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
@@ -20,8 +22,23 @@ class ProfileController extends Controller
         ]);
     }
 
-    public function update()
+    public function update(UpdateProfileRequest $request)
     {
-
+        $user = Auth::user();
+        if ($request->hasFile('profile_pic')) {
+            if (Storage::exists('public/' . $user->profile_pic)) {
+                Storage::delete('public/' . $user->profile_pic);
+            }
+            $path = $request->file('profile_pic')->store('image', 'public');
+            $relativePath = str_replace('public/', '', $path);
+            $data = $request->except('profile_pic');
+            $data['profile_pic'] = $relativePath;
+        } else {
+            $data = $request->except('profile_pic');
+        }
+        if ($user->update($data)) {
+            return redirect('/')->with('success', 'the profile is updated successfully');
+        }
+        return redirect('/')->with('warning', 'something went wrong.try again');
     }
 }
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -9,10 +9,10 @@
     <section class="home">
         <div class="home-content">
             <h1>Hi,I'm {{$user->firstName.' '.$user->lastName}}</h1>
-            <h3>Resume: web developer</h3>
-            <p class="about">About: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolorem dolores error
-                exercitationem expedita maiores, molestias nisi, nulla obcaecati officia optio possimus quibusdam
-                quisquam quo repellat, sequi similique velit veniam?</p>
+            @if($user->user_type === 'seeker')
+                <h3>Resume: {{$user->resume}}</h3>
+            @endif
+            <p class="about">About: {{$user->about}}</p>
             <p>Email: {{$user->email}}</p>
             <p>Account Type: {{$user->user_type}}</p>
             @if($user->user_type === 'employee')
@@ -24,6 +24,8 @@
         </div>
         <div class="home-image">
             <div class="glowing-circle">
+                <span></span>
+                <span></span>
                 <div class="image">
                     <img src="{{\Illuminate\Support\Facades\Storage::url($user->profile_pic ?? 'image/Default-profile.png')}}" alt="">
                 </div>
