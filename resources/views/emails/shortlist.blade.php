5c503c7b685e6b12d7dfba51d632a2a9df5d9084
Amirhossein <Amirmemool12@gmail.com>
2023-08-20 00:44:22 +0330

    :heavy_plus_sign: feat: create ShortlistMail

---
+++ resources/views/emails/shortlist.blade.php
@@ -0,0 +1,11 @@
+<x-mail::message>
+# Introduction
+
+You added to the Shortlist.Prepare for the Interview
+<p>Job Title: {{$post->title}}</p>
+<p>Job Description: {{$post->description}}</p>
+<p>Job Address: {{$post->address}}</p>
+
+Thanks,<br>
+From Find Job
+</x-mail::message>
