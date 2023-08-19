48199b12c461381be43a5089e577bb22d66c3945
Amirhossein <Amirmemool12@gmail.com>
2023-08-19 23:32:25 +0330

    :hammer: chore: difine a route to shortlist the user

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,7 @@ Route::group(['prefix' => 'job','middleware' => 'isPremium'], function () {
     Route::delete('/delete',[PostJobController::class,'delete'])->middleware(IsYourPost::class);
     Route::get('/applicants/{post}', [ApplicantController::class,'index'])->middleware(IsYourPost::class);
     Route::delete('/applicants/delete', [ApplicantController::class,'deleteUser'])->middleware(IsYourPost::class);
+    Route::get('/applicants/shortlist/{post}/{userId}', [ApplicantController::class,'shortlist'])->middleware(IsYourPost::class);
 });
 //profile group
 Route::group(['prefix' => 'profile','middleware' => 'auth'], function () {
