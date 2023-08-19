0d338b4ad1ed3e5edbf5e6d8712679fd0d988442
Amirhossein <Amirmemool12@gmail.com>
2023-08-20 00:14:48 +0330

    :lipstick: style: change the title of applicant table

--- resources/views/job/applicants.blade.php
+++ resources/views/job/applicants.blade.php
@@ -5,11 +5,11 @@
     <div id="layoutSidenav_content" style="margin-left: -200px">
         <main>
             <div class="container-fluid px-4">
-                <h1 class="mt-4">Jobs</h1>
+                <h1 class="mt-4">Applicants</h1>
                 <div class="card mb-4">
                     <div class="card-header">
                         <i class="fas fa-table me-1"></i>
-                        Your job table
+                        Applicants of Your job
                     </div>
                     <div class="card-body">
                         <table id="datatablesSimple">
@@ -18,6 +18,7 @@
                                 <th>First Name</th>
                                 <th>Last Name</th>
                                 <th>Email</th>
+                                <th>Shortlist</th>
                                 <th>Action</th>
                             </tr>
                             </thead>
@@ -26,26 +27,39 @@
                                 <th>First Name</th>
                                 <th>Last Name</th>
                                 <th>Email</th>
+                                <th>Shortlist</th>
                                 <th>Action</th>
                             </tr>
                             </tfoot>
-                            <tbody>
+                            <tbody >
                             @foreach($applicants as $applicant)
-                                <tr>
-                                    <td>{{$applicant->firstName}}</td>
-                                    <td>{{$applicant->lastName}}</td>
-                                    <td>{{$applicant->email}}</td>
-                                    <td>
-                                        <a style="color: #157347;font-size: 16px;padding: 7px 12px;text-decoration: none;background: #dee2e6" href="/profile/user/{{$applicant->id}}">Profile</a>
-                                        <form  style="display: inline"  action="/job/applicants/delete" method="post">
-                                            @csrf
-                                            @method("DELETE")
-                                            <input type="hidden" name="delete" value="{{$post->id}}">
-                                            <input type="hidden" name="user" value="{{$applicant->id}}">
-                                            <button style="border: 8px;height: 30px;color: red;background: #dee2e6" type="submit">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
+                            <tr>
+                                <td>{{$applicant->firstName}}</td>
+                                <td>{{$applicant->lastName}}</td>
+                                <td>{{$applicant->email}}</td>
+                                <td>
+                                    @if($applicant->pivot->shortlisted)
+                                        Added
+                                    @else
+                                        Not Added
+                                    @endif
+                                </td>
+                                <td>
+                                    @if($applicant->pivot->shortlisted)
+                                        <a class="black-button" href="/job/applicants/unShortlist/{{$post->id}}/{{$applicant->id}}">Remove from shortlist</a>
+                                    @else
+                                        <a class="green-button" href="/job/applicants/shortlist/{{$post->id}}/{{$applicant->id}}">Add to shortlist</a>
+                                    @endif
+                                        <a class="blue-button" style="color: #FFFFFF;background: #007bff;font-size: 16px;padding: 7px 12px;border-radius: 5px;text-decoration: none;" href="/profile/user/{{$applicant->id}}">Profile</a>
+                                    <form  style="display: inline"  action="/job/applicants/delete" method="post">
+                                        @csrf
+                                        @method("DELETE")
+                                        <input type="hidden" name="delete" value="{{$post->id}}">
+                                        <input type="hidden" name="user" value="{{$applicant->id}}">
+                                        <button class="red-button"  type="submit">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
                             @endforeach
                             </tbody>
                         </table>
