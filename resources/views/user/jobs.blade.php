a922befa34524eced2b13e5131f2a0edeaa2951d
Amirhossein <Amirmemool12@gmail.com>
2023-08-20 18:50:30 +0330

    :lipstick: style: add more style:

--- resources/views/user/jobs.blade.php
+++ resources/views/user/jobs.blade.php
@@ -39,25 +39,25 @@
         <div class="row mt-2 g-1">
             @foreach($jobs as $job)
                 <div class="col-md-3">
-                    <div class="card p-1 d-flex flex-column h-100">
+                    <div class="card p-1 d-flex flex-column h-100 job-card">
                         <div class="text-right"><small class="badge text-bg-info">{{$job->job_type}}</small></div>
                         <div class="text-center mt-2 p-3 h-100">
                             <div style="height: 135px">
                                 <img class="rounded-circle" src="{{\Illuminate\Support\Facades\Storage::url($job->post_image ?? 'image/Default-Profile.png')}}" width="100" height="100" alt="Not loaded"/> <br>
                                 <span class="d-block"
-                                      style="font-weight: bold;color: black;font-size: 23px">{{$job->user->company}}</span>
+                                      style="font-weight: bold;color: white;font-size: 23px">{{$job->user->company}}</span>
                                 </div>
                             <hr style="margin: 0 0;">
                             <div style="height: 70px">
-                                <span style="color: black;font-weight: bold;font-size: 19px">{{$job->title}}</span><br>
-                                <span style="color: black;font-size: 13px;">{{$job->description}}</span>
+                                <span style="color: white;font-weight: bold;font-size: 19px">{{$job->title}}</span><br>
+                                <span style="color: white;font-size: 13px;">{{$job->description}}</span>
                             </div>
                             <div style="height: 10px">
-                                <small class="ml-1" style="">Address: {{$job->address}}</small>
+                                <small class="ml-1" style="color: white">Address: {{$job->address}}</small>
                             </div>
-                            <div class="d-flex justify-content-between mt-3"><span style=";color: black;font-size: 20px">${{$job->salary}}</span>
+                            <div class="d-flex justify-content-between mt-3"><span style=";color: white;font-size: 20px">${{$job->salary}}</span>
                                 <a href="#">
-                                    <button class="btn btn-dark">Apply Now</button>
+                                    <button class="btn btn-dark" >Apply Now</button>
                                 </a>
                             </div>
                         </div>
@@ -69,17 +69,17 @@
         <div class="row mt-2">
             <div class="col-md-12 d-flex justify-content-center">
                 @if ($jobs->currentPage() > 1)
-                    <a href="{{ $jobs->previousPageUrl()}}" class="btn btn-success">Previous</a>
+                    <a href="{{ $jobs->previousPageUrl()}}" class="btn btn-dark">Previous</a>
                 @endif
                 @if ($jobs->lastPage() > 1)
                     <div class="btn-group" style="margin: 0 5px;">
                         @for ($i = 1; $i <= $jobs->lastPage(); $i++)
-                            <a href="{{ $jobs->url($i) }}" class="btn btn-success {{ $i == $jobs->currentPage() ? 'active' : '' }}">{{ $i }}</a>
+                            <a href="{{ $jobs->url($i) }}" class="btn btn-dark {{ $i == $jobs->currentPage() ? 'active' : '' }}">{{ $i }}</a>
                         @endfor
                     </div>
                 @endif
                 @if ($jobs->hasMorePages())
-                    <a href="{{ $jobs->nextPageUrl() }}" class="btn btn-success">Next</a>
+                    <a href="{{ $jobs->nextPageUrl() }}" class="btn btn-dark">Next</a>
                 @endif
             </div>
         </div>
