6af56fd5d6707c5855c299e1d3e10e77f3d6cbdc
Amirhossein <Amirmemool12@gmail.com>
2023-08-20 16:56:38 +0330

    :hammer: chore: pass all the job to fornt

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\Post;
+
 class JobController extends Controller
 {
     public function index()
     {
-        return view('user.jobs');
+        $posts = Post::where('close_date','>',now()->format('Y-m-d'));
+        return view('user.jobs',[
+            'jobs'=>$posts
+        ]);
     }
 }
