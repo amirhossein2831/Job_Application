b30b737e353134354688a04d0fc96680bd295e5d
Amirhossein <Amirmemool12@gmail.com>
2023-08-20 16:23:51 +0330

    :heavy_plus_sign: feat: create view to show jobs to user

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -7,6 +7,6 @@ class JobController extends Controller
 {
     public function index()
     {
-
+        return view('user.jobs');
     }
 }
---
+++ resources/views/user/jobs.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('contact')
+    <h1>Hello jobs</h1>
+@endsection
