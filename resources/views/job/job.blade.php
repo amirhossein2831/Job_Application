0fe7812fec864793387a42fe4c6d6af4ba243eb3
Amirhossein <Amirmemool12@gmail.com>
2023-08-21 14:32:51 +0330

    :lipstick: style: put the job info in the job page

--- resources/views/job/job.blade.php
+++ resources/views/job/job.blade.php
@@ -4,23 +4,23 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <img src="https://placehold.co/800" class="card-img-top" alt="Cover Image"
-                         style="height: 150px; object-fit: cover;">
+                    <img
+                        src="{{\Illuminate\Support\Facades\Storage::url($job->post_image ?? 'image/Default-Profile.png')}}"
+                        class="card-img-top" alt="Cover Image"
+                        style="height: 200px; object-fit: cover;">
                     <div class="card-body">
-                        <h2 class="card-title">Job Title</h2>
-                        <p>this job provide from  <span class="badge bg-gradient">{{$job->user->company}}</span></p>
-                        <p>Job Type: <span class="badge bg-primary">Full-time</span></p>
-                        <p>Salary: </p>
-                        <p>Address: </p>
+                        <h2 class="card-title">{{$job->title}}</h2>
+                        <p>this job provide from <span class="badge bg-gradient">{{$job->user->company}}</span></p>
+                        <h4>Info</h4>
+                        <p>Job Type: <span class="badge bg-primary">{{$job->job_type}}</span></p>
+                        <p>Salary: ${{$job->salary}} </p>
+                        <p>Address: {{$job->address}} </p>
                         <h4 class="mt-4">Description</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pretium
-                            eleifend erat id finibus. Donec tristique purus vitae urna varius, sed laoreet nisl
-                            efficitur.</p>
+                        <p class="card-text">{{$job->description}}</p>
 
                         <h4>Roles and Responsibilities</h4>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolorum facere fuga ipsum minima molestiae quidem rerum tempora totam ut!
-
-                        <p class="card-text mt-4">Application closing date: March 31, 2023</p>
+                        {{$job->roles}}
+                        <p class="card-text mt-4">Application closing date: {{$job->close_date}}</p>
 
                         <a href="#" class="btn btn-primary mt-3">Apply Now</a>
                     </div>
