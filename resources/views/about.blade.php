d46453d2612cfda2f6a6801d50e6a885316a7d55
Amirhossein <Amirmemool12@gmail.com>
2023-08-22 22:54:42 +0330

    :lipstick: style: create about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('contact')
+    <div id="container" class="wrapper" style="height: auto;width: 800px">
+        <div class="form-box login">
+            <h1 style="font-size: 50px">Find Job</h1>
+            <h3>The Best place to find job and also find worker</h3>
+            <div style=";margin-top: 10px">
+                <h5>Register now.</h5>
+            </div>
+            <div style="text-align: left;margin-top: 60px">
+                <p>You can find job,create profile and add your resume,profile picture,about and ...</p>
+                <p>You can also apply for the job that you want</p>
+                <p>you can post your job and find some employee to work together</p>
+                <p>for who want to find job every thing is free</p>
+                <p>for who want to post job there is 7 days free trial</p>
+            </div>
+            <div style="margin-top: 50px">
+                <p>the idea behind this project coma from AmirHossein Motaghian.junior web developer who love to make project and learn more</p>
+            </div>
+            <div style="margin-top: 40px">
+                <p><a href="mailto:Amirmemool12@gmail.com">mail --> AmirHossein Motaghian</a></p>
+                <p><a href="https://github.com/amirhossein2831">GIT HUB --> AmirHossein Motaghian</a></p>
+            </div>
+        </div>
+    </div>
+@endsection
