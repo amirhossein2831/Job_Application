3b7ca52d0c3b7a0ef6903261853e5a52e5842856
Amirhossein <Amirmemool12@gmail.com>
2023-08-22 22:24:01 +0330

    :lipstick: style: design the ui of apply mail

---
+++ resources/views/emails/apply.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+# Introduction
+
+someone Apply to your job
+
+<p>{{$userName}} applied to your job.go and checkout applicants of the below job</p>
+<p>Job Title:{{$jobTitle}}</p>
+<p>Date: {{$date}}</p>
+
+Thanks,<br>
+From Find Job
+</x-mail::message>
