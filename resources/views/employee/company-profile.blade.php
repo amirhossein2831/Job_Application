5e6e67f7a7a14ff509a6bd1a8b1dc94782a6c453
Amirhossein <Amirmemool12@gmail.com>
2023-08-23 15:01:47 +0330

    :lipstick: style: design the company UI

--- resources/views/employee/company-profile.blade.php
+++ resources/views/employee/company-profile.blade.php
@@ -2,43 +2,46 @@
 
 @section('contact')
 
-<div class="container jobs">
-    <div class="row justify-content-center mt-2">
-        <div class="col">
-            <div class="hero-section" style="background-color:#f5f5f5;width:100%;height:200px;">
-                <!-- <img src="" style="width: 100%; height:250px;"> -->
-            </div>
-        </div>
-    </div>
-
-    <div class="row mt-5">
-        <div class="col">
-            <img src="" alt="Company Logo" class="img-fluid">
-            <h2>Company Name</h2>
-        </div>
-    </div>
-
-    <div class="row mt-5">
-        <div class="col">
-            <h3>About</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pretium eleifend erat id finibus. Donec tristique purus vitae urna varius, sed laoreet nisl efficitur.</p>
-        </div>
-    </div>
-
-    <div class="row mt-5">
-        <div class="col-md-8">
-            <h3>List of Jobs</h3>
-
-            <div class="card mb-3">
-                <div class="card-body">
-                    <h5 class="card-title">Job Title 1</h5>
-                    <p class="card-text">Location: </p>
-                    <p class="card-text">Salary: $5000 per year</p>
-                    <a href="#" class="btn btn-primary">View</a>
+    <div class="container jobs">
+        <div class="row justify-content-center mt-2">
+            <div class="col">
+                <div class="hero-section" style="background-color:#f5f5f5;width:100%;height:400px;">
+                    <img src="{{Storage::url($company->profile_pic)}}"
+                         style="width: 100%;object-fit: cover; height:400px;">
                 </div>
             </div>
         </div>
+
+        <div class="row mt-5">
+            <div style="display: inline-flex">
+                <img style="width: 50px;height: 50px" src="{{Storage::url($company->profile_pic)}}" alt="Company Logo"
+                     class="rounded-circle">
+                <h2 style="margin-left: 10px;margin-top: 5px">{{$company->company}}</h2>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col">
+                <h3>About</h3>
+                <p>{{$company->about}}</p>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <h3>List of Jobs</h3>
+                @foreach($jobs as $job)
+                    <div class="card mb-3 job-card">
+                        <div class="card-body">
+                            <h5 class="card-title">Title: {{$job->title}}</h5>
+                            <p class="card-text">Location: {{$job->address}}</p>
+                            <p class="card-text">Salary: ${{$job->salary}} per year</p>
+                            <a href="/jobs/{{$job->id}}" class="btn btn-outline-light">View</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
-</div>
 
 @endsection
